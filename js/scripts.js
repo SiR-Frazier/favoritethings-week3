1840c35445cc7767c54760d3396d336fb6ec9590
Bryce Frazier and Ron Craig <ron@>
2018-01-16 10:53:37 -0800

    final commit return new array based on user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,23 @@
 $(document).ready (function() {
-  $("#favorite-things").submit(function(event) {
-  var favoriteThings = ['favoriteColor', 'favoriteMovie', 'favoriteDessert', 'favoriteSport'];
+  $("#blanks form").submit(function(event) {
+
   var favoriteColor = $("input#fav-color").val();
   var favoriteMovie = $("input#fav-movie").val();
   var favoriteDessert = $("input#fav-dessert").val();
   var favoriteSport = $("input#fav-sport").val();
 
-    $(".fav-color").text(favoriteColor);
-    $(".fav-movie").text(favoriteMovie);
-    $(".fav-dessert").text(favoriteDessert);
-    $(".fav-sport").text(favoriteSport);
-    
-    event.preventDefault();
+  var favoriteThings = [favoriteColor, favoriteMovie, favoriteDessert, favoriteSport];
+
+  var pushNewArray = [];
+  pushNewArray.push(favoriteColor);
+  pushNewArray.push(favoriteMovie);
+  pushNewArray.push(favoriteDessert);
+
+  $(".fav-color").text(pushNewArray[0]);
+  $(".fav-movie").text(pushNewArray[1]);
+  $(".fav-dessert").text(pushNewArray[2]);
+
+  $(".newArray").show();
+  event.preventDefault();
   });
 });
